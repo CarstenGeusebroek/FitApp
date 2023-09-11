1d805da83b434c2cc7b39676de81e6f6b3bc2e9b
Carsten <83281084+CarstenGeusebroek@users.noreply.github.com>
2023-09-11 12:50:02 +0000

    sidenavbar-bmi

---
+++ js/script.js
@@ -0,0 +1,46 @@
+function pageManager(pageId) {
+
+    //Pagina's
+    var firstPage = document.getElementsByClassName('Homepage')[0];
+    var secondPage = document.getElementsByClassName('CalVerbrand')[0];
+    var thridPage = document.getElementsByClassName('BMIBerekenen')[0];
+
+    switch(pageId) {
+        case 0: 
+            pageOff(secondPage);
+            pageOn(firstPage);
+            pageOff(thridPage);
+            break;
+        case 1: 
+            pageOff(firstPage);
+            pageOn(secondPage);
+            pageOff(thridPage);
+            break;
+        case 2: 
+            pageOff(firstPage);
+            pageOff(secondPage);
+            pageOn(thridPage)
+            break;
+        default:
+            console.error("page id bestaat niet");
+    }
+}
+
+//Zet pagina uit
+function pageOff(x) {
+    x.style.display = "none";
+}
+
+//Zet pagina aan
+function pageOn(x) {
+    x.style.display = "block";
+}
+
+function BMIBerekenen() {
+    var lengte = document.getElementsByClassName("lengteBMI")[0].value / 100;
+    var gewicht = document.getElementsByClassName("gewichtBMI")[0].value;
+    var output = document.getElementById("BMIOutput");
+    console.log(gewicht / (lengte * lengte));
+    output.innerHTML = gewicht / (lengte * lengte);
+
+}
