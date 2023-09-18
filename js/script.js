6301563b3a0a1c494579e2d4f298029801a6be97
Carsten <83281084+CarstenGeusebroek@users.noreply.github.com>
2023-09-18 06:25:48 +0000

    bmi-gezondheid-meter

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ function pageManager(pageId) {
 
     switch (pageId) {
         case 0:
-            pageOff(secondPage);
             pageOn(firstPage);
+            pageOff(secondPage);    
             pageOff(thridPage);
             break;
         case 1:
@@ -39,8 +39,23 @@ function pageOn(x) {
 function BMIBerekenen() {
     var lengte = document.getElementsByClassName("lengteBMI")[0].value / 100;
     var gewicht = document.getElementsByClassName("gewichtBMI")[0].value;
-    var output = document.getElementById("BMIOutput");
+    
+    var bmi = Math.round((gewicht / (lengte * lengte) * 10)) / 10;
     console.log(gewicht / (lengte * lengte));
-    output.innerHTML = "Jouw bmi is: " + Math.round((gewicht / (lengte * lengte) * 10)) / 10;
+    IsGezondBMI(bmi)
+}
 
+function IsGezondBMI(bmi) {
+    var output = document.getElementById("BMIOutput");
+    var gezondheid;
+    if(bmi <= 20) {
+        gezondheid = " Uw heeft ondergewicht. Het is belangrijk dat u gewichtstoename realiseert.";
+    } else if(bmi <=  25) {
+        gezondheid = " Uw gewicht is gezond. Probeer op dit gewicht te blijven.";
+    } else if(bmi <= 30) {
+        gezondheid = " U heeft overgewicht. Afvallen is verstandig.";
+    } else {
+        gezondheid = " Uw gewicht is levensbedreigend. Neem contact op met uw huisarts.";
+    }
+    output.innerHTML = "Jouw bmi is: " + bmi + "." + gezondheid;
 }
