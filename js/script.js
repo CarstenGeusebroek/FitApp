ab126342049cc702ae7aeac9564be0dcba612cfc
Carsten <83281084+CarstenGeusebroek@users.noreply.github.com>
2023-09-19 18:42:34 +0000

    calverbrand teller

--- js/script.js
+++ js/script.js
@@ -66,7 +66,7 @@ function IsGezondBMI(bmi) {
 
 function onCalVerbrand() {
     var activiteit = document.getElementById("activiteitInput");
-    var tijd = Number(document.getElementById("tijdInput")) / 60;
+    var tijd = Number(document.getElementById("tijdInput").value) / 60;
     console.log(tijd);
     switch (activiteit.value) {
         case "wandelen":
@@ -85,5 +85,36 @@ function onCalVerbrand() {
 }
 
 function berekenCal(perUur, aantalUur) {
-    console.log(perUur * aantalUur);
+    var output = document.getElementById("calOutput");
+    var calVerbrand = perUur * aantalUur;
+
+
+    const date = new Date();
+    var today = formatDate(date);
+    if (localStorage.getItem("lastSavedDay") !== today) {
+        //Vandaag nog niet gesaved
+        localStorage.setItem("lastSavedDay", today);
+        localStorage.setItem("calToday", calVerbrand);
+    } else {
+        //Vandaag al wel gesaved
+        localStorage.setItem("calToday", Number(localStorage.getItem("calToday")) + calVerbrand);
+    }
+
+    var calVerbrandVandaag = localStorage.getItem("calToday");
+    var highscore = Number(localStorage.getItem("dagHighScore"));
+    console.log(highscore);
+
+    if (highscore < calVerbrandVandaag) {
+        output.innerHTML = `Nieuwe highscore van de dag!!! Je hebt nu ${calVerbrand} calorieën verbrand. Je zit vandaag al op ${calVerbrandVandaag} calorieën, goed bezig!`;
+        localStorage.setItem("dagHighScore", calVerbrandVandaag);
+    } else {
+        output.innerHTML = `Je hebt nu ${calVerbrand} calorieën verbrand. Je zit vandaag al op ${calVerbrandVandaag} calorieën, goed bezig! Nog ${highscore - calVerbrand} en dan heb je een nieuw highscore!`;
+    }
+}
+
+
+
+function formatDate(date) {
+    var correctDate = date.getDate().toString() + (date.getMonth() + 1).toString() + date.getFullYear().toString();
+    return correctDate;
 }
